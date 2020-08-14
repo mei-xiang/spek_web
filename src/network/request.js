34a07f1aa10799d117ef68eaf6c688f7786c683a
梅强 <1443636033@qq.com>
2020-08-14 09:57:26 +0800

    样式微调

--- src/network/request.js
+++ src/network/request.js
@@ -65,7 +65,7 @@ instance.interceptors.response.use(response => {
   if (err && err.response) {
     switch (err.response.status) {
       case 400:
-        console.log('错误请求')
+        // console.log('错误请求')
         break;
       case 401:
         setTimeout(() => {
@@ -79,40 +79,40 @@ instance.interceptors.response.use(response => {
         break;
         break;
       case 403:
-        console.log('拒绝访问')
+        // console.log('拒绝访问')
         break;
       case 404:
-        console.log('网络请求不存在')
+        // console.log('网络请求不存在')
         break;
       case 405:
-        console.log('请求方法未允许')
+        // console.log('请求方法未允许')
         break;
       case 408:
-        console.log('请求超时')
+        // console.log('请求超时')
         break;
       case 500:
-        console.log('服务器端出错')
+        // console.log('服务器端出错')
         break;
       case 501:
-        console.log('网络未实现')
+        // console.log('网络未实现')
         break;
       case 502:
-        console.log('网络错误')
+        // console.log('网络错误')
         break;
       case 503:
-        console.log('服务不可用')
+        // console.log('服务不可用')
         break;
       case 504:
-        console.log('网络超时')
+        // console.log('网络超时')
         break;
       case 505:
-        console.log('http版本不支持该请求')
+        // console.log('http版本不支持该请求')
         break;
       default:
-        console.log(`连接错误${err.response.status}`)
+        // console.log(`连接错误${err.response.status}`)
     }
   } else {
-    console.log('连接到服务器失败')
+    // console.log('连接到服务器失败')
     // 处理断网的情况
     // eg:请求超时或断网时，更新state的network状态
     // network状态在app.vue中控制着一个全局的断网提示组件的显示隐藏
